af6a4a02300e7aed04f2e56cf692f1063ac49f72
Edgar Herrera <edgarinelfuerte@hotmail.com>
2020-06-14 16:31:23 -0500

    Added database connection property to interface.

--- Interface.py
+++ Interface.py
@@ -1,15 +1,17 @@
 import Empresa
+import MysqlConnection
 
 
-# clase Interfaz
+# clase Interface
 class Interface:
+    connection: None
+
     def __init__(self):
         self.empresas = []
         self.empresasCount = len(self.empresas)
 
-    def setEmpresa(self,nombreEmpresa : str,direccionEmpresa : str,rfcEmpresa: str):
-
-        empresa = Empresa.Empresa(nombreEmpresa, direccionEmpresa, rfcEmpresa,self.empresasCount)
+    def setEmpresa(self, nombreEmpresa: str, direccionEmpresa: str, rfcEmpresa: str):
+        empresa = Empresa.Empresa(nombreEmpresa, direccionEmpresa, rfcEmpresa, self.empresasCount)
         self.empresas.append(empresa)
 
     def getLastEmpresa(self) -> Empresa:
@@ -20,3 +22,10 @@ class Interface:
     def getEmpresas(self):
         return self.empresas
 
+    def startConnection(self, eleccion: str):
+        if eleccion == '1':
+            self.connection = MysqlConnection.MysqlConnection.get_instance()
+            # esperando la clase de ubaldo
+            # elif eleccion == '2':
+            # self.connection = clase de ubaldo para mongodb
+            pass
